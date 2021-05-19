ce8986c091c41cd31629a0133246d49a99adbf8c
STZG <1600337300@qq.com>
2021-05-20 01:47:34 +0800

    成绩查询

--- myzstu-uni/src/pages/edu/score/score.vue
+++ myzstu-uni/src/pages/edu/score/score.vue
@@ -1,7 +1,7 @@
 <template>
   <view class="container">
-    <picker mode="selector" v-model="yearindex" :range="years">
-      <view class="picker">
+    <picker mode="selector" :value="yearindex" :range="years" @change="pickerChange">
+      <view class="picker" v-if="years[yearindex] !=  undefined ">
         {{years[yearindex]}}学期<image class="down" src="/static/images/down.svg"></image>
       </view>
     </picker>
@@ -14,11 +14,13 @@
         <view class="gpa">绩点</view>
       </view>
       <view class="noscore" v-if="scorelist.length < 1">暂无成绩信息</view>
-      <view class="content" v-for="item in scorelist" :key="item.courseName">
-        <view class="name">{{item.courseName}}</view>
-        <view class="credit">{{item.credit}}</view>
-        <view class="grade">{{item.grade}}</view>
-        <view class="gpa">{{item.gpa}}</view>
+      <view class="score-item" v-for="item in scorelist" :key="item.courseName">
+        <view class="content" v-if="item.xnmmc + '-' + item.xqmmc == years[yearindex]">
+          <view class="name">{{item.kcmc}}</view>
+          <view class="credit">{{item.xf}}</view>
+          <view class="grade">{{item.cj}}</view>
+          <view class="gpa">{{item.jd}}</view>
+        </view>
       </view>
     </view>
     <view class="scoresum">
@@ -54,32 +56,116 @@
 
 <script>
   const app = getApp()
+
+  import {
+    getScores
+  } from '@/api/edu.js'
+
   export default {
     data() {
       return {
-        years: wx.getStorageSync('years'),
         yearindex: 0,
-        scorelist: [],
-        credit_semester: 0,
-        avggpa_semester: 0.00.toFixed(2),
-        credit_year: 0,
-        avggpa_year: 0.00.toFixed(2),
-        credit_all: 0,
-        avggpa_all: 0.00.toFixed(2),
+        scorelist: []
       }
+    },
+    computed: {
+      years() {
+        let y = new Set()
+        if (this.scorelist.length > 0) {
+          this.scorelist.forEach(item => {
+            y.add(item.xnmmc + '-' + item.xqmmc)
+          })
+        }
+        return [...y].sort().reverse()
+      },
+      credit_all() {
+        return this.scorelist.reduce((t, v) =>
+          v.ksxzdm == "01" ? t + parseFloat(v.xf) : t, 0)
+      },
+      avggpa_all() {
+        return (this.scorelist.reduce((t, v) =>
+          v.ksxzdm == "01" ? t + parseFloat(v.xfjd) : t, 0) / this.credit_all).toFixed(2)
+      },
+      credit_year() {
+        var picker = this.years[this.yearindex]
+        if (picker == undefined) {
+          return 0.00
+        }
+        var year = picker.split('-')[0] + '-' + picker.split('-')[1]
+        return this.scorelist.reduce((t, v) => {
+          if (year == v.xnmmc && v.ksxzdm == "01") {
+            return t + parseFloat(v.xf)
+          } else {
+            return t
+          }
+        }, 0)
+
+      },
+      avggpa_year() {
+        var picker = this.years[this.yearindex]
+        if (picker == undefined) {
+          return 0.00
+        }
+        var year = picker.split('-')[0] + '-' + picker.split('-')[1]
+        var xfjd = this.scorelist.reduce((t, v) => {
+          if (year == v.xnmmc && v.ksxzdm == "01") {
+            return t + parseFloat(v.xfjd)
+          } else {
+            return t
+          }
+        }, 0)
+        return (xfjd / this.credit_year).toFixed(2)
+      },
+      credit_semester() {
+        var picker = this.years[this.yearindex]
+        if (picker == undefined) {
+          return 0.00
+        }
+        return this.scorelist.reduce((t, v) => {
+          if (picker == v.xnmmc + '-' + v.xqmmc && v.ksxzdm == "01") {
+            return t + parseFloat(v.xf)
+          } else {
+            return t
+          }
+        }, 0)
+      },
+      avggpa_semester() {
+        var picker = this.years[this.yearindex]
+        if (picker == undefined) {
+          return 0.00
+        }
+        var xfjd = this.scorelist.reduce((t, v) => {
+          if (picker == v.xnmmc + '-' + v.xqmmc && v.ksxzdm == "01") {
+            return t + parseFloat(v.xfjd)
+          } else {
+            return t
+          }
+        }, 0)
+        return (xfjd / this.credit_semester).toFixed(2)
+      }
+    },
+    mounted() {
+      uni.showLoading({
+        title: '拼命加载中...',
+      })
+      getScores().then(scores => {
+        this.scorelist = scores
+        uni.hideLoading()
+      }).catch(err => {
+        uni.hideLoading()
+        uni.showToast({
+          title: '无法获取成绩信息，请稍后再试',
+          icon: 'none',
+          duration: 1500
+        })
+      })
     },
     methods: {
-
-    },
-    bindValueChange: function(e) {
-      this.setData({
-        yearindex: e.detail.value
-      })
-      var scores = wx.getStorageSync('scores')
-      if (scores != null) {
-        this.showScores(scores)
+      pickerChange: function(e) {
+        this.yearindex = e.detail.value
       }
     },
+
     /**
      * 生命周期函数--监听页面加载
      */
@@ -98,103 +184,9 @@
      * 生命周期函数--监听页面显示
      */
     onShow: function() {
-      var that = this
-      var openid = wx.getStorageSync('openid')
-      if (openid == '') {
-        wx.showToast({
-          title: '微信登陆失败',
-          icon: 'none',
-          duration: 1500
-        })
-      } else {
-        wx.showLoading({
-          title: '拼命加载中...',
-          mask: true
-        })
-        wx.request({
-          url: app.globalData.serverUrl + '/wx/grades',
-          method: 'GET',
-          header: {
-            'content-type': 'application/x-www-form-urlencoded'
-          },
-          data: {
-            openid: openid
-          },
-          success: function(res) {
-            wx.hideLoading()
-            if (res.data.code == "201") {
-              wx.showToast({
-                title: '学号错误,请重新绑定',
-                icon: 'none',
-                duration: 1000,
-                success: function() {
-                  setTimeout(function() {
-                    wx.navigateTo({
-                      url: '../../mine/userform/userform'
-                    })
-                  }, 1500)
-                }
-              })
-            } else if (res.data.code == "202") {
-              wx.showToast({
-                title: '教务系统密码错误,请重新绑定',
-                icon: 'none',
-                duration: 1000,
-                success: function() {
-                  setTimeout(function() {
-                    wx.navigateTo({
-                      url: '../../mine/userform/userform'
-                    })
-                  }, 1500)
-                }
-              })
-            } else if (res.data.code == '200') {
-              if (res.data.data.length > 0) {
-                var years = res.data.years.reverse()
-                that.setData({
-                  years: years
-                })
-                wx.setStorageSync('years', years)
-                wx.setStorageSync('scores', res.data)
-                that.showScores(res.data)
-              }
-            } else {
-              that.getDataFromStorage()
-            }
-          },
-          fail: function(err) {
-            wx.hideLoading()
-            that.getDataFromStorage()
-          }
-        })
-      }
 
     },
-    getDataFromStorage() {
-      wx.showLoading({
-        title: '当前查询人数过多，将尝试读取本地缓存',
-      })
-      var that = this
-      wx.getStorage({
-        key: 'scores',
-        success: function(res) {
-          wx.hideLoading()
-          that.setData({
-            yearindex: 0,
-            years: res.data.years
-          })
-          that.showScores(res.data)
-        },
-        fail: function(err) {
-          wx.hideLoading()
-          wx.showToast({
-            title: '本地暂无缓存，请稍后再试',
-            icon: 'none',
-            duration: 1500
-          })
-        }
-      })
-    },
+
     /**
      * 生命周期函数--监听页面隐藏
      */
@@ -228,55 +220,6 @@
      */
     onShareAppMessage: function() {
 
-    },
-    showScores: function(res) {
-      var picker = res.years[this.data.yearindex]
-      var year = picker.split('-')[0] + '-' + picker.split('-')[1]
-      var term = picker.split('-')[2]
-      var scorelist = []
-      var data = res.data
-      var credit_semester = 0,
-        avggpa_semester = 0,
-        credit_year = 0,
-        avggpa_year = 0,
-        credit_all = 0,
-        avggpa_all = 0
-      var credit = 0,
-        gpa = 0,
-        credit_and_pga_semester = 0,
-        credit_and_pga_year = 0,
-        credit_and_pga_all = 0
-      for (var i = 0; i < data.length; i++) {
-        if (data[i].credit.trim()) {
-          credit = parseFloat(data[i].credit)
-        }
-        if (data[i].gpa.trim()) {
-          gpa = parseFloat(data[i].gpa)
-        }
-        credit_all += credit
-        credit_and_pga_all += gpa * credit
-        if (data[i].year == year) {
-          credit_year += credit
-          credit_and_pga_year += gpa * credit
-          if (data[i].term == term) {
-            credit_semester += credit
-            credit_and_pga_semester += gpa * credit
-            scorelist.push(data[i])
-          }
-        }
-      }
-      avggpa_semester = credit_and_pga_semester / credit_semester
-      avggpa_year = credit_and_pga_year / credit_year
-      avggpa_all = credit_and_pga_all / credit_all
-      this.setData({
-        scorelist: scorelist,
-        credit_semester: credit_semester.toFixed(0),
-        avggpa_semester: avggpa_semester.toFixed(2),
-        credit_year: credit_year.toFixed(0),
-        avggpa_year: avggpa_year.toFixed(2),
-        credit_all: credit_all.toFixed(0),
-        avggpa_all: avggpa_all.toFixed(2),
-      })
     }
   }
 </script>
