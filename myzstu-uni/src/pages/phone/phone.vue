37a3322e6dd52db09b1c400738da4699fbc73ed9
STZG <1600337300@qq.com>
2021-05-19 22:18:02 +0800

    常用电话

--- myzstu-uni/src/pages/phone/phone.vue
+++ myzstu-uni/src/pages/phone/phone.vue
@@ -1,198 +1,177 @@
 <template>
-	<view>
-		<view class="header">
-		</view>
-		<view class="container" v-if="phone">
-		  <view class="category">生活类</view>
-		  <view v-for="item in life" :key="item.name" class="item" @tap='call'>
-		    <view class="name">{{item.name}}</view>
-		    <view class="number"  >{{item.number}}</view>
-		  </view>
-		  <view class="category">办公类</view>
-		  <view v-for="item in work" :key="item.name" class="item" @tap='call'>
-		    <view class="name">{{item.name}}</view>
-		    <view class="number">{{item.number}}</view>
-		  </view>
-		</view>
-	</view>
+  <view>
+    <view class="header">
+    </view>
+    <view class="container">
+      <view class="category" v-if="life.length != 0">生活类</view>
+      <view v-for="item in life" :key="item.number" class="item" @tap='call'>
+        <view class="name">{{item.name}}</view>
+        <view class="number">{{item.number}}</view>
+      </view>
+      <view class="category" v-if="work.length != 0">办公类</view>
+      <view v-for="item in work" :key="item.number" class="item" @tap='call'>
+        <view class="name">{{item.name}}</view>
+        <view class="number">{{item.number}}</view>
+      </view>
+      <view class="category" v-if="other.length != 0">其他</view>
+      <view v-for="item in other" :key="item.number" class="item" @tap='call'>
+        <view class="name">{{item.name}}</view>
+        <view class="number">{{item.number}}</view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
   const app = getApp()
-	export default {
-		data() {
-			return {
-				phone: false,
-				life: [],
-				work: [],
-				other: []
-			}
-		},
-		methods: {
-			/**
-			 * 自定义函数
-			 */
-			showNumber(data) {
-			  var life = []
-			  var work = []
-			  //var other = []
-			  for (var i = 0; i < data.length; i++) {
-			    if (data[i].type == 'life') {
-			      life.push(data[i])
-			    }
-			    else if (data[i].type == 'work') {
-			      work.push(data[i])
-			    }
-			    /*else {
-			      other.push(data[i])
-			    }*/
-			  }
-			  wx.setStorageSync('life', life)
-			  wx.setStorageSync('work', work)
-			  //wx.setStorageSync('other', other)
-			  this.setData({
-			    phone: wx.getStorageSync('phone'),
-			    life: life,
-			    work: work,
-			    //other: other
-			  })
-			},
-			call (e) {
-			  var number = e.currentTarget.dataset.item.number
-			  wx.makePhoneCall({
-			    phoneNumber: number,
-			  })
-			}
-		},
-    
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-      
+  export default {
+    data() {
+      return {
+        life: [],
+        work: [],
+        other: []
+      }
     },
-    
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-      
-    },
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-      var that=this
+    mounted() {
       try {
-        var phone = wx.getStorageSync('phone')
-        if (phone == true) {
-          this.setData({
-            phone: phone,
-            life: wx.getStorageSync('life'),
-            work: wx.getStorageSync('work'),
-            //other: wx.getStorageSync('other')
-          })
-        }else{
-          wx.request({
-            url: app.globalData.serverUrl + '/wx/phones',
-            method: 'GET',
-            success: function (res) {
-              if(res.statusCode == 200 && res.data.data != null){
-                console.log(app.globalData.serverUrl + '/phones'),
-                console.log(res);
-                wx.setStorageSync('phone', true)
-                that.showNumber(res.data.data)
-              }else{
-                wx.setStorageSync('phone', false)
-                wx.showToast({
-                  title: '加载失败',
-                  icon: 'none',
-                  duration: 1500
-                })
-              }
-            },
-            fail: function (err) {
-              wx.setStorageSync('phone', false)
-              wx.showToast({
+        var phones = uni.getStorageSync('phones')
+        if (phones != null && phones != '') {
+          this.showNumber(phones)
+        } else {
+          uniCloud.callFunction({
+            name: 'phones'
+          }).then(res => {
+            if (res.result.code == 0 && res.result.phones != null) {
+              console.log(res);
+              uni.setStorageSync('phones', res.result.phones)
+              this.showNumber(res.result.phones)
+            } else {
+              uni.showToast({
                 title: '加载失败',
                 icon: 'none',
                 duration: 1500
               })
-    
             }
+          }).catch((err) => {
+            uni.showToast({
+              title: '加载失败',
+              icon: 'none',
+              duration: 1500
+            })
           })
         }
       } catch (e) {
         console.log(e);
       }
     },
-    
+    methods: {
+      /**
+       * 自定义函数
+       */
+      showNumber(data) {
+        this.life = data.life
+        this.work = data.work
+        this.other = data.other
+      },
+      call(e) {
+        var number = e.currentTarget.dataset.item.number
+        uni.phoneNumber = number
+      }
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function () {
-      
+    onHide: function() {
+
     },
-    
+
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function () {
-      
+    onUnload: function() {
+
     },
-    
+
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
-      
+    onPullDownRefresh: function() {
+
     },
-    
+
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {
-      
+    onReachBottom: function() {
+
     },
-    
+
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
-      
+    onShareAppMessage: function() {
+
     }
-	}
+  }
 </script>
 
-<style>
-  page{
-    background:#FFF;
+<style lang="scss">
+  page {
+    background: #FFF;
     padding-bottom: 15rpx;
   }
-  .header{
+
+  .header {
     height: 15rpx;
   }
-  .container{
-    width:100%; 
-  }
-  .category{
-    font-size:45rpx;
-    text-align:left;
-    padding:20rpx;
-    color:#000;
-  }
-  .item{
-    width:90%;
-    color:#000;
-    display: flex;
-    text-align:center;
-    background:#fff;
-    border:1px solid #31c27c;
-    border-radius:10rpx;
-    padding:20rpx;
-    margin-bottom:15rpx;
-  }
-  .item .name{
-    margin-right:auto;
+
+  .container {
+    width: 100%;
+
+    .category {
+      font-size: 45rpx;
+      text-align: left;
+      padding: 20rpx;
+      color: #000;
+    }
+
+    .item {
+      width: 90%;
+      color: #000;
+      display: flex;
+      text-align: center;
+      background: #fff;
+      border: 1px solid #31c27c;
+      border-radius: 10rpx;
+      padding: 20rpx;
+      margin-left: 4rpx;
+      margin-bottom: 16rpx;
+
+      .name {
+        margin-right: auto;
+      }
+    }
   }
 </style>
